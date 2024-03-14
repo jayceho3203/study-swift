77ff83aab2f8d6eb3cc237c0f7b7816b970a838f
Jayce Ho <jayce.hodang@gmail.com>
2024-03-15 00:21:27 +0700

    cmm

---
+++ proj-day2/proj-day2/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  proj-day2
+//
+//  Created by Jayce Ho on 15/03/2024.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ proj_day1/proj_day1/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  proj_day1
+//
+//  Created by Jayce Ho on 15/03/2024.
+//
+
+import Foundation
+
+print("Hello, World!")
+
