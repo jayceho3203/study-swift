5dbdfb006c9d834a71a10111ba1ac0457f99485b
Jayce Ho <jayce.hodang@gmail.com>
2024-03-15 14:56:10 +0700

    arrays

--- proj-day3/arrays.playground/Contents.swift
+++ proj-day3/arrays.playground/Contents.swift
@@ -1,4 +1,13 @@
 import UIKit
 
 var beatles = ["Dat", "Nguyen", "Ryhan", "Jayce"]
+var numbers = [1 , 2, 99, 50.5]
+var temparature = [20.5, 30, 30.5]
+
+beatles.append("Huy Huynh")
+print(beatles[0])
+print(beatles[1])
+print(beatles[2])
+print(beatles[3])
+
 
