30400d2faff1e9aeafc55a78f58c4081fd9f49be
Jayce Ho <jayce.hodang@gmail.com>
2024-03-21 20:19:57 +0700

    ternary conditional operator

---
+++ proj-day5(conditions)/ternary-conditional-operator.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// IN STEAD OF:
+let hour = 12
+if hour >= 12 {
+    print("It's after noon")
+} else {
+    print("It's before noon")
+}
+
+
+// you can write
+print(hour >= 12 ? "It's after noon" : "It's before noon")
+
+let names = ["Jayce", "Nguyen", "Ut"]
+let crewCount = names.isEmpty ? "No one" : "\(names.count) people"
+print(crewCount)
+
+enum Theme {
+    case dark, light
+}
+
+let theme = Theme.dark
+
+let background = theme == .dark ? "black" : "white"
+
+print(background)
