f80a277f34e76baa653b7701bd39b4f83c2708a9
Jayce Ho <jayce.hodang@gmail.com>
2024-04-21 20:24:15 +0700

    func

--- proj-day7(func-pt1)/function.playground/Contents.swift
+++ proj-day7(func-pt1)/function.playground/Contents.swift
@@ -40,6 +40,7 @@ var root =  pythagoras(a: 3, b: 4)
 print(root)
 
 //  RETURN MULTIPLE VALUES
+// Array
 func getUser1() -> [String] {
     ["Jayce", "Ho"]
 }
@@ -47,6 +48,7 @@ func getUser1() -> [String] {
 let user1 = getUser1()
 print(user1, user1[0], user1[1])
 
+//dictionary
 func getUser2() -> [String : Any] {
     ["firstname": "jayce",
       "lastname": "ho"]
@@ -55,7 +57,23 @@ let user2 = getUser2()
 print("user's firstname: \(user2["firstname", default: "nknowned"])")
 print("user's age: \(user2["age", default: "unknowned"])")
 
-func getUser3() -> (firstname: string, lastname: string, age: Int){
-    (firstname: "jayce", lastname: "ho", age:21)
+//tuple
+func getUser3() -> (firstname: String, lastname: String, age: Int){
+    (firstname: "jayce",
+     lastname: "ho",
+     age:21)
     
 }
+
+let user3 = getUser3()
+let firstname = user3.firstname
+let lastname = user3.lastname
+print("3rd user's first name: \(firstname)")
+print("3rd user's last name: \(lastname)")
+print("3rd user's full name: \(firstname) \(lastname)")
+
+let (firstName, lastName, age) = getUser3()
+print("Name: \(firstName) \(lastName)")
+
+let (ten, _, _) = getUser3()
+print("first name: \(ten)")
