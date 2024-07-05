b0e75a31602c653da9c2bf485d710e2d503d4e7a
Jayce Ho <jayce.hodang@gmail.com>
2024-07-05 22:34:38 +0700

    access control

--- proj-day11/accessControl.playground/Contents.swift
+++ proj-day11/accessControl.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+struct BankAccount {
+    private(set) var funds = 0
+    
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw (amount: Int) -> Bool{
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+        
+    }
+}
+var account = BankAccount()
+account.deposit(amount: 1000)
+print(account.funds)
+// account.funds += 2000 -> error
+
+var success = account.withdraw(amount: 1500)
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Not enough money ")
+}
